56576d73381f1341a81f43bd14097cdc4584cc2b
Arsibal97 <asierralaya@gmail.com>
2024-08-20 19:17:00 +0200

    finished header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { DialogoFotoComponent } from "../components/dialogo-foto/dialogo-foto.component";
+import { ModoNocheComponent } from "../modo-noche/modo-noche.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, DialogoFotoComponent],
+  imports: [HeaderComponent, DialogoFotoComponent, ModoNocheComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/modo-noche/modo-noche.component.ts
+++ src/app/modo-noche/modo-noche.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component, Renderer2, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-modo-noche',
   standalone: true,
@@ -8,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './modo-noche.component.css'
 })
 export class ModoNocheComponent {
+  private isDarkMode = false;
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit() {
+    // Inicializa el estado del interruptor basándose en la clase del body
+    const body = this.el.nativeElement.ownerDocument.body;
+    this.isDarkMode = body.classList.contains('tema-oscuro');
+    this.updateSwitchState();
+  }
+
+  cambio(): void {
+    const body = this.el.nativeElement.ownerDocument.body;
+
+    // Alternar entre tema claro y oscuro
+    if (this.isDarkMode) {
+      this.renderer.removeClass(body, 'tema-oscuro');
+      this.renderer.addClass(body, 'tema-claro');
+    } else {
+      this.renderer.removeClass(body, 'tema-claro');
+      this.renderer.addClass(body, 'tema-oscuro');
+    }
+
+    // Alternar el estado del interruptor
+    this.isDarkMode = !this.isDarkMode;
+    this.updateSwitchState();
+  }
+
+  private updateSwitchState(): void {
+    // Actualizar el estado del interruptor basado en la variable isDarkMode
+    const switchElement = this.el.nativeElement.querySelector('.switch') as HTMLInputElement;
+    if (switchElement) {
+      switchElement.checked = this.isDarkMode;
+    }
+  }
 }
